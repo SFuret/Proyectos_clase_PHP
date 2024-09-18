71b6542eeaf7109fb844b7bafcc706e8d6e55a2a
Sandra <sanfurdia@alu.edu.gva.es>
2024-09-18 16:05:18 +0200

    Cambio1

---
+++ bienvenido.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido a PHP</title>
+</head>
+<body>
+   <?php echo '<h1>Bienvenido a esta página</h1>' ?> 
+        </body>
+</html>
