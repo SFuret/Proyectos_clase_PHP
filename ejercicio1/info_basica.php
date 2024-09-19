329a66965ac14a4ba7a9025b78c33e65afc1de30
Sandra <sanfurdia@alu.edu.gva.es>
2024-09-19 17:54:02 +0200

    cambio1

---
+++ ejercicio1/info_basica.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis datos</title>
+</head>
+<body>
+    <h1>Datos personales</h1>
+
+     <?php $nombre="Sandra";
+     $anyoNac=1985;
+
+     ?>
+     <p>Me llamo <?php echo $nombre; ?>  y nací en <?php echo $anyoNac; ?></p>   
+      
+</body>
+</html>
