e95efd651f5803a7bb6d9986d5206823c3e19c52
Sandra <sanfurdia@alu.edu.gva.es>
2024-09-19 18:56:42 +0200

    cambio3

--- ejercicio1/area_circulo.php
+++ ejercicio1/area_circulo.php
@@ -8,12 +8,15 @@
 <body>
     <h1>Área círculo</h1>
     <?php
-    define('PI', 3.1416);
+    define('PI', 3.1416);  //declaración de una constante
     $radio=3.5;
     $area= PI* pow($radio,2);  /*La función pow() es para calcular la potencia*/
-    echo "El área del círculo es $area"; /*si uso " " en el echo me lee el contenido de las variables,
+
+    echo "El área del círculo es $area <br>"; /*si uso " " en el echo me lee el contenido de las variables,
     con ' ' toma las variables como texto*/
 
+    $areaRedondeada= round($area,2); /*Redondea a dos posiciones decimales, hacia arriba*/
+    echo "El área del círculo redondeada es $areaRedondeada";
     ?>
     
 </body>
