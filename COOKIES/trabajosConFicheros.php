cae6cbc3c0def4d7324fafee09a1327c77c19d60
Sandra <sanfurdia@alu.edu.gva.es>
2024-11-25 18:14:06 +0100

    trabajo con cookies

---
+++ COOKIES/trabajosConFicheros.php
@@ -0,0 +1,72 @@
+<?php 
+$coches=[
+    'coche1'=>['precio'=>'15000', 'marca'=>'Toyota'],
+    'coche2'=>['precio'=>'20000', 'marca'=>'Audi'],
+    'coche3'=>['precio'=>'30000', 'marca'=>'Mercedes']
+];
+
+foreach($coches as $clave=>$valor)
+{
+  setcookie($clave,$valor['marca'],time()+3600, "/", "localhost");  
+}
+
+//echo $_COOKIE['coche1'];
+
+//guardar el array en fichero1
+
+//convierto el array a json
+$cochesJson= json_encode($coches, JSON_PRETTY_PRINT);
+
+//guardo el json en un fichero
+
+$fichero="fichero1.json";
+try{
+if(!file_exists($fichero))
+{
+  throw new Exception("No existe el fichero");
+}
+else{
+
+  //GUARDAR EL JSON EN UN FICHERO
+  $resultado= file_put_contents($fichero,$cochesJson);
+
+  if($resultado==true)
+  {
+  echo "Datos guardados en fichero1";
+  }
+  else{
+    echo "Error al guardar los datos";
+  }
+
+}
+}catch(Exception $e)
+{
+ echo $e->getMessage();
+}
+
+
+//LEER LOS DATOS GUARDADOS EN UN FICHERO JSON
+
+
+  try{
+    if(!file_exists($fichero))
+    {
+      throw new Exception("No existe el fichero");
+    }
+    else{
+      $contenidoJson= file_get_contents($fichero);
+      //convertir el json en un arrayAsociativo
+
+      $arrayCoches= json_decode($contenidoJson, true);
+      var_dump($arrayCoches);
+
+
+    }
+  }catch(Exception $e)
+  {
+    $e->getMessage();
+  }
+
+
+
+?>
---
+++ Formularios/formulario1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario 1</title>
+</head>
+<body>
+    
+<form action="" method="POST">
+ <fieldset>
+    <legend>Formulario 1</legend>
+ <label for="inmuebles">Tipo de inmuebles</label>
+ <select name="inmuebles" id="inmuebles">
+    <option value="piso">Piso</option>
+    <option value="adosado">Adosado</option>
+    <option value="chalet">Chalet</option>
+ </select>
+
+
+ </fieldset>
+</form>
+
+</body>
+</html>
